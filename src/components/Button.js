c3efcfb942fa5dd1e74c6c0410c85c554fd0bbf0
Gareth <gar.carey@gmail.com>
2021-11-11 20:33:01 -0500

    add conditionals for which type of button will be used

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,24 @@
+import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-  return null;
+  let buttonClass = "button";
+
+  if (props.confirm) {
+    buttonClass += " button--confirm";
+  }
+
+  if (props.danger) {
+    buttonClass += " button--danger";
+  }
+
+  return (
+    <button
+      className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
 }
