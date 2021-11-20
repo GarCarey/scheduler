1c93d4fb729749b4de1b6ba6218eeea88df0507a
Gareth <gar.carey@gmail.com>
2021-11-20 09:52:29 -0500

    add import of getInterview function + apply this within Application function

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,6 +4,7 @@ import DayList from "./DayList";
 import "components/Appointment";
 import "components/Application.scss";
 import Appointment from "components/Appointment";
+import { getAppointmentsForDay, getInterview } from "../helpers/selectors";
 
 /*const appointments = {
   1: {
@@ -50,21 +51,38 @@ export default function Application(props) {
     day: "Monday",
     days: [],
     appointments: {},
+    interviewers: {},
   });
 
-  const setDay = (day) => setState({ ...state, day });
-  const setDays = (days) => setState((prev) => ({ ...prev, days }));
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
 
-  let appointmentsArray = Object.values(state.appointments).map(
-    (appointment) => {
-      return <Appointment key={appointment.id} {...state.appointment} />;
-    }
-  );
+  const setDay = (day) => setState({ ...state, day });
+
+  let appointmentsArray = dailyAppointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    );
+  });
+
+  const dayApi = axios.get("http://localhost:8001/api/days");
+  const appApi = axios.get("http://localhost:8001/api/appointments");
+  const interviwerApi = axios.get("http://localhost:8001/api/interviewers");
 
   useEffect(() => {
-    axios.get("http://localhost:8001/api/days").then((response) => {
-      console.log(response.data);
-      setDays(response.data);
+    Promise.all([dayApi, appApi, interviwerApi]).then((all) => {
+      setState((prev) => ({
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data,
+      }));
     });
   }, []);
 
