024e9872e0a82f88bddf7b2bad11cda4ad2defab
Gareth <gar.carey@gmail.com>
2021-11-25 22:33:13 -0500

    import useVisualMode + add transitions to header and form

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,18 +3,30 @@ import Header from "components/Appointment/header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
 import "components/Appointment/styles.scss";
+import useVisualMode from "hooks/useVisualMode";
+import Form from "./Form";
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? (
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === CREATE && (
+        <Form student="" interviewers={props.interviewers} onCancel={back} />
+      )}
+      {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
         />
-      ) : (
-        <Empty />
       )}
     </article>
   );
